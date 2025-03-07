045650a59f2400055a57eefe128ca00de95545a0
Victor Novais <victor96novais@gmail.com>
2025-03-06 21:40:55 -0300

    feat: responsive layout

--- src/presentation/screens/Home/styles.ts
+++ src/presentation/screens/Home/styles.ts
@@ -1,49 +1,49 @@
 import styled from "styled-components/native";
-import { DefaultTheme } from "styled-components";
+import { scale, verticalScale, moderateScale } from "react-native-size-matters";
 
 export const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.primary};
-  padding: 16px;
+  padding: ${moderateScale(16)}px;
 `;
 
 export const Header = styled.View`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  padding-top: 32px;
-  padding-bottom: 16px;
+  padding-top: ${moderateScale(28)}px;
+  padding-bottom: ${moderateScale(16)}px;
 `;
 
 export const HeaderText = styled.Text`
-  font-size: 24px;
+  font-size: ${moderateScale(24)}px;
   font-weight: bold;
   color: ${({ theme }) => theme.colors.textColor};
 `;
 
 export const ProfileIcon = styled.Image`
-  width: 50px;
-  height: 50px;
-  border-radius: 25px;
+  width: ${scale(50)}px;
+  height: ${verticalScale(50)}px;
+  border-radius: ${moderateScale(25)}px;
 `;
 
 export const MealCard = styled.TouchableOpacity`
   background-color: ${({ theme }) => theme.colors.primary};
-  padding-top: 16px;
-  padding-bottom: 10px;
-  border-radius: 10px;
+  padding-top: ${moderateScale(16)}px;
+  padding-bottom: ${moderateScale(10)}px;
+  border-radius: ${moderateScale(10)}px;
   flex-direction: row;
   align-items: center;
 `;
 
 export const MealImage = styled.Image`
-  width: 60px;
-  height: 60px;
-  border-radius: 10px;
-  margin-right: 10px;
+  width:${scale(60)}px;
+  height: ${verticalScale(60)}px;
+  border-radius: ${moderateScale(10)}px;
+  margin-right: ${moderateScale(10)}px;
 `;
 
 export const MealText = styled.Text`
-  font-size: 16px;
+  font-size: ${moderateScale(16)}px;
   color: ${({ theme }) => theme.colors.textColor};
 `;
 
